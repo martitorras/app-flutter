4401867cf34e29d42ab8d4b4ac234206b1ba7507
martitorras <marti.torras98@gmail.com>
2019-10-15 20:10:58 +0200

    buttons and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:exercise/zone.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(ZoneInfoApp());
@@ -49,7 +50,102 @@ class _Buttons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      color: Colors.grey[100],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                width: 180,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'View sub-areas',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  color: Colors.blue[800],
+                ),
+              ),
+              Container(
+                width: 180,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'Search routes',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  color: Colors.blue[800],
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                width: 180,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'Area Map',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  color: Colors.blue[800],
+                ),
+              ),
+              Container(
+                width: 180,
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.file_download,
+                      color: Colors.white,
+                      size: 20.0,
+                    ),
+                    Text(
+                      'Download',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  color: Colors.blue[800],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -87,30 +183,111 @@ class _Image extends StatelessWidget {
           Expanded(
             flex: 2,
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Icon(
-                  Icons.bookmark_border,
-                  color: Colors.grey,
-                  size: 40.0,
+                /*SizedBox(
+                  width: 10,
+                ),*/
+                Column(
+                  children: <Widget>[
+                    SizedBox(height: 5),
+                    Container(
+                      child: Center(
+                        child: Icon(
+                          Icons.bookmark_border,
+                          color: Colors.grey[400],
+                          size: 30.0,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Bookmark',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(width: 60),
-                 Icon(
-                  Icons.favorite,
-                  color: Colors.grey,
-                  size: 40.0,
+                VerticalDivider(
+                  color: Colors.grey[400],
                 ),
-                SizedBox(width: 60),
-                 Icon(
-                  Icons.favorite,
-                  color: Colors.grey,
-                  size: 40.0,
+                Column(
+                  children: <Widget>[
+                    SizedBox(height: 5),
+                    Container(
+                      child: Center(
+                        child: Text(
+                          '${zone.toDos}',
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'To-Dos',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(width: 60),
-                 Icon(
-                  Icons.favorite,
-                  color: Colors.grey,
-                  size: 40.0,
+                VerticalDivider(
+                  color: Colors.grey[400],
                 ),
+                Column(
+                  children: <Widget>[
+                    SizedBox(height: 5),
+                    Container(
+                      child: Center(
+                        child: Text(
+                          '${zone.ticks}',
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Ticks',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+                VerticalDivider(
+                  color: Colors.grey[400],
+                ),
+                Column(
+                  children: <Widget>[
+                    SizedBox(height: 5),
+                    Container(
+                      child: Center(
+                        child: Icon(
+                          Icons.share,
+                          color: Colors.grey[400],
+                          size: 30.0,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Share',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+                /*SizedBox(
+                  width: 10,
+                ),*/
               ],
             ),
           )
